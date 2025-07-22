424123bce3c3f82a95c547df2f406e52606f5994
amarouane-ABDELHAK <am0089@uah.edu>
2025-07-22 12:47:36 -0500

    Change color

--- src/components/universal/TopBar.js
+++ src/components/universal/TopBar.js
@@ -4,11 +4,11 @@ import React from 'react'
 const TopBar = () => {
 
   return (
-    <AppBar position="relative" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, bgcolor: '#d83933', minHeight:  "92px", boxShadow: "none", display: 'flex', justifyContent: 'center', fontFamily: "'Inter', sans-serif", fontWeight: 800}}>
+    <AppBar position="relative" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, bgcolor: '#082a64', minHeight:  "92px", boxShadow: "none", display: 'flex', justifyContent: 'center', fontFamily: "'Inter', sans-serif", fontWeight: 800}}>
         <Toolbar>
           <a href="/">
         <Box
-            sx={{width: 120}}
+            sx={{width: 70}}
             component='img'
             src='/browseui/assets/us-ghg-logo.svg'/>
             </a>
